025f01cba97f206f97520199d29da051428383d8
Gunasekhar Ravilla <14607@ramco.com>
2023-02-28 12:54:06 +0530

    features

--- src/components/features/FeatureGallery.js
+++ src/components/features/FeatureGallery.js
@@ -1,15 +1,21 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import img1 from '../heroSection/jcb.png';
 import img2 from '../heroSection/agri.png';
 import img3 from '../heroSection/jcb1.png';
+import tipper from './tipper.png';
+import freight from './freight.png';
+import van from './van.png';
 import './FeatureGallery.scss';
+import backhoe from './backhoe.png';
+import backhoe2 from './backhoe2.png';
 
 const TABS = [
 	{
-		title: 'Lorry',
+		title: 'Tipper',
 		description:
 			'<p>Transport heavy goods in a large truck that can carry up to 30 tons.</p>',
-		imageUrl: 'https://example.com/lorry-image.png',
+		imageUrl: [tipper],
+
 		baseColor: '50,50,50',
 		secondColor: '200,200,200'
 	},
@@ -17,7 +23,7 @@ const TABS = [
 		title: 'Tractor',
 		description:
 			'<p>Transport goods and materials with a tractor that can pull heavy loads and equipment.</p>',
-		imageUrl: 'https://example.com/tractor-image.png',
+		imageUrl: freight,
 		baseColor: '31,143,183',
 		secondColor: '20,91,115'
 	},
@@ -25,7 +31,7 @@ const TABS = [
 		title: 'Truck',
 		description:
 			'<p>Transport goods and materials in a medium or heavy-duty truck that can carry up to 20 tons.</p>',
-		imageUrl: 'https://example.com/truck-image.png',
+		imageUrl: freight,
 		baseColor: '255,204,0',
 		secondColor: '255,180,0'
 	},
@@ -33,7 +39,7 @@ const TABS = [
 		title: 'Backhoe Loader',
 		description:
 			'<p>Move earth and materials with a versatile backhoe loader that can dig, lift, and load.</p>',
-		imageUrl: 'https://example.com/backhoe-loader-image.png',
+		imageUrl: [backhoe, backhoe2],
 		baseColor: '51,102,153',
 		secondColor: '38,76,114'
 	},
@@ -41,7 +47,7 @@ const TABS = [
 		title: 'Van',
 		description:
 			'<p>Transport small to medium-sized goods and materials in a van that can carry up to 3 tons.</p>',
-		imageUrl: 'https://example.com/van-image.png',
+		imageUrl: van,
 		baseColor: '38,50,56',
 		secondColor: '18,23,26'
 	}
@@ -55,112 +61,76 @@ const imagesMap = {
 export default function FeaturesGallery() {
 	const [currentTab, setCurrentTab] = useState(TABS[0]);
 
-	// const imagesMarkup = TABS.map((singleTab, idx) => {
-	// 	const isActive = singleTab.title === currentTab.title;
-	// 	const isFirst = idx === 0;
-
-	// 	return (
-	// 		<div
-	// 			key={singleTab.title}
-	// 			className={`image-container ${isActive ? 'active' : ''}`}
-	// 		>
-	// 			<img src={singleTab.imageUrl} alt={singleTab.title} />
-	// 		</div>
-	// 	);
-	// });
-
-	// const tabsMarkup = TABS.map((singleTab, idx) => {
-	// 	const isActive = singleTab.title === currentTab.title;
-
-	// 	return (
-	// 		<div
-	// 			className={`tab ${isActive ? 'active' : ''}`}
-	// 			key={idx}
-	// 			onClick={() => handleTabClick(idx)}
-	// 		>
-	// 			<div className="tab-title-container">
-	// 				<div className="circle-container">
-	// 					<div
-	// 						className="three-layers-circle"
-	// 						style={{
-	// 							backgroundColor: isActive
-	// 								? 'transparent'
-	// 								: `rgb(${singleTab.baseColor})`
-	// 						}}
-	// 					>
-	// 						<div
-	// 							className="inner-circle"
-	// 							style={{ backgroundColor: `rgb(${singleTab.secondColor})` }}
-	// 						/>
-	// 					</div>
-	// 				</div>
-	// 				<h4>{singleTab.title}</h4>
-	// 			</div>
-	// 			<div className={`tab-content ${isActive ? 'open' : ''}`}>
-	// 				<div
-	// 					dangerouslySetInnerHTML={{ __html: singleTab.description }}
-	// 				></div>
-	// 			</div>
-	// 		</div>
-	// 	);
-	// });
+	// automatically update the current tab in particular time interval
+	useEffect(() => {
+		const interval = setInterval(() => {
+			const currentIdx = TABS.findIndex(
+				(singleTab) => singleTab.title === currentTab.title
+			);
+			const nextIdx = currentIdx + 1 === TABS.length ? 0 : currentIdx + 1;
+			setCurrentTab(TABS[nextIdx]);
+		}, 5000);
+		return () => clearInterval(interval);
+	}, [currentTab]);
 
 	function handleTabClick(idx) {
 		setCurrentTab(TABS[idx]);
 	}
 
 	return (
-		<div className="FeaturesGalleryWrapper ">
-			<div className="GalleryWrapper">
-				<div className="tabs-container">
-					{/* loop the Tabs */}
-					{TABS.map(function (singleTab, idx) {
-						const isActive = singleTab.title === currentTab.title;
-						return (
-							<>
-								<div
-									className={`tab ${isActive ? 'active' : ''}`}
-									key={idx}
-									onClick={() => handleTabClick(idx)}
-								>
-									<div className="tab-title-container">
-										<div className="circle-container">
-											<div
-												className="three-layers-circle"
-												style={{
-													backgroundColor: isActive
-														? 'transparent'
-														: `rgb(${singleTab.baseColor})`
-												}}
-											>
-												<div
-													className="inner-circle"
-													style={{
-														backgroundColor: `rgb(${singleTab.secondColor})`
-													}}
-												/>
-											</div>
-										</div>
-										<h4>{singleTab.title}</h4>
-									</div>
-									<div className={`tab-content ${isActive ? 'open' : ''}`}>
+		<div className="GalleryWrapper">
+			<div className="tabs-container">
+				{TABS.map(function (singleTab, idx) {
+					const isActive = singleTab.title === currentTab.title;
+					return (
+						<div
+							className={`tab ${isActive ? 'active' : ''}`}
+							key={idx}
+							onClick={() => handleTabClick(idx)}
+						>
+							<div className="tab-title-container">
+								<div className="circle-container">
+									<div
+										className="three-layers-circle"
+										style={{
+											backgroundColor: isActive
+												? 'transparent'
+												: `rgb(${singleTab.baseColor})`
+										}}
+									>
 										<div
-											dangerouslySetInnerHTML={{
-												__html: singleTab.description
+											className="inner-circle"
+											style={{
+												backgroundColor: `rgb(${singleTab.secondColor})`
 											}}
 										></div>
 									</div>
 								</div>
-							</>
-						);
-					})}
-				</div>
-				{
-					// render currentTab
-					<div className="image-container">
+								<h4>{singleTab.title}</h4>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+			<div className="image-container">
+				{/* if currentTab.imageUrl is an array render random image */}
+				{Array.isArray(currentTab.imageUrl) ? (
+					<div className={currentTab.title}>
+						<img
+							src={
+								currentTab.imageUrl[
+									Math.floor(Math.random() * currentTab.imageUrl.length)
+								]
+							}
+							alt={currentTab.title}
+							className={currentTab.title}
+						/>
+					</div>
+				) : (
+					<div className={currentTab.title}>
 						<img src={currentTab.imageUrl} alt={currentTab.title} />
 					</div>
-				}
+				)}
 			</div>
 		</div>
 	);
--- src/components/heroSection/Hero.js
+++ src/components/heroSection/Hero.js
@@ -4,13 +4,13 @@ import heroimage from './heroimage.png';
 
 const Hero = () => {
 	return (
-		<div class="hero-wrapper">
-			<div class="contents">
-				<h2 class="custom-over-title">
+		<div className="hero-wrapper">
+			<div className="contents">
+				<h2 className="custom-over-title">
 					Welcome to <span>ALL WHEELS</span>
 				</h2>
-				<h1 class="heading">The Ultimate Vehicle Destination</h1>
-				<p class="description">
+				<h1 className="heading">The Ultimate Vehicle Destination</h1>
+				<p className="description">
 					Introducing our user-friendly booking app, designed to make the
 					process of renting Agricultural, Transport and construction equipment
 					as simple and hassle-free as possible. With just a few taps on your
@@ -18,13 +18,13 @@ const Hero = () => {
 					loaders, trucks, lorries, and other equipment, and book the one that
 					suits your project requirements.
 				</p>
-				<div class="custom-button-group">
-					<a href="#whitepaper" class="button transparent">
+				<div className="custom-button-group">
+					<a href="#whitepaper" className="button transparent">
 						Features <span>&rarr;</span>
 					</a>
 				</div>
 			</div>
-			<div class="image-container">
+			<div className="image-container">
 				<img src={heroimage} alt="heroimage" />
 			</div>
 		</div>
