797b62f5573bbe2fe412dc6105168d2a48412572
Gunasekhar Ravilla <14607@ramco.com>
2023-03-04 20:47:43 +0530

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,50 +1,164 @@
 import React from 'react';
+import logo from '../header/Logo1.png';
 import './Footer.scss';
 
 function Footer() {
-	const socialLinks = [
-		{
-			id: 1,
-			label: 'Facebook',
-			url: 'https://www.facebook.com/ravilla.guna'
-		},
-		{ id: 2, label: 'Twitter', url: 'https://twitter.com/' },
-		{
-			id: 3,
-			label: 'Instagram',
-			url: 'https://www.instagram.com/gunasekharravilla/'
+	const footerData = {
+		logo: logo,
+		socialLinks: [
+			{
+				name: 'Facebook',
+				icon: 'fab fa-facebook-f',
+				link: 'https://www.facebook.com'
+			},
+			{
+				name: 'Twitter',
+				icon: 'fab fa-twitter',
+				link: 'https://www.twitter.com'
+			},
+			{
+				name: 'Instagram',
+				icon: 'fab fa-instagram',
+				link: 'https://www.instagram.com'
+			}
+		],
+		routes: [
+			{
+				title: 'About Us',
+				links: [
+					{
+						title: 'Our Story',
+						url: '/about/story'
+					},
+					{
+						title: 'Leadership',
+						url: '/about/leadership'
+					},
+					{
+						title: 'Careers',
+						url: '/about/careers'
+					}
+				]
+			},
+			{
+				title: 'Services',
+				links: [
+					{
+						title: 'Service 1',
+						url: '/services/service1'
+					},
+					{
+						title: 'Service 2',
+						url: '/services/service2'
+					},
+					{
+						title: 'Service 3',
+						url: '/services/service3'
+					}
+				]
+			}
+		],
+		copyRight: '© 2023 All rights reserved.',
+		otherLinks: [
+			{
+				title: 'Privacy Policy',
+				url: '/privacy'
+			},
+			{
+				title: 'Terms of Use',
+				url: '/terms'
+			}
+		],
+		dropdown: {
+			title: 'Select Country',
+			icon: 'fa fa-chevron-circle-down  ',
+			options: ['USA', 'Canada', 'Australia', 'United Kingdom']
 		}
-	];
+	};
+
 	return (
-		<footer className="footer" id="contact">
-			<div className="container">
-				<div className="row">
-					<div className="col-md-6">
-						<ul className="list-inline">
-							{socialLinks.map((link) => (
-								<li key={link.id} className="list-inline-item">
-									{/* open link in new tab */}
+		<footer className="footer">
+			<div className="footerContainer" id="footerContainer">
+				<div className="footerTopContainer">
+					<div className="socialContainer">
+						<div className="web">
+							<img src={footerData.logo || ''} alt="All Wheels" />
+							<div className="social_links">
+								{footerData?.socialLinks?.map((item) => (
 									<a
-										href={link.url}
+										href={item.link}
+										className="event-elem"
 										target="_blank"
-										rel="noopener noreferrer"
-										className={link.label?.toLowerCase()}
+										key={item.name}
+										rel="noreferrer"
 									>
-										{link.label}
+										{/* fontawesome icons */}
+										<i className={item.icon}></i>
+									</a>
+								))}
+							</div>
+						</div>
+						{footerData?.routes?.map((route) => (
+							<div className="routeContainer" key={route.title}>
+								<ul>
+									{route?.links?.map((link) => (
+										<li key={link.title}>
+											<a href={link.url} className="event-elem">
+												{link.title}
+											</a>
+										</li>
+									))}
+								</ul>
+							</div>
+						))}
+					</div>
+				</div>
+
+				<div className="divider web"></div>
+
+				<div className="footerBottomContainer">
+					<div className="desc web">{footerData?.copyRight || ''}</div>
+					<div className="otherlinks">
+						<ul>
+							{footerData?.otherLinks?.map((link) => (
+								<li key={link.title}>
+									<a href={link.url} className="event-elem">
+										{link.title}
 									</a>
 								</li>
 							))}
+							<li className="web">
+								<div className="pull-right">
+									<div className="country-select" id="country-select">
+										<div
+											className="select"
+											onClick={() => {
+												document
+													.getElementById('country-options')
+													.classList.toggle('hide');
+											}}
+										>
+											<i className={footerData?.dropdown?.icon}></i>
+											<div className="selected">
+												{footerData?.dropdown?.title}
+											</div>
+										</div>
+										<ul
+											className="select-options country-options hide"
+											id="country-options"
+										>
+											{footerData?.dropdown?.options.map((option) => (
+												<li key={option}>{option}</li>
+											))}
+										</ul>
+									</div>
+								</div>
+							</li>
 						</ul>
-					</div>
-					<div className="col-md-6 text-md-right">
-						<p>
-							&copy; {new Date().getFullYear()} All Wheels. All rights reserved.
-						</p>
 					</div>
 				</div>
 			</div>
 		</footer>
 	);
 }
-
 export default Footer;
